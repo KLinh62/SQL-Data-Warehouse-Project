e4da651f2be1009b39197622f429e7559a533071
KLinh62 <nklinh.620@gmail.com>
2025-06-15 15:01:54 +0700

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -88,7 +88,7 @@ GO
   
 CREATE TABLE silver.erp_px_cat_g1v2 (
 	id NVARCHAR(50),
-	category NVARCHAR(50),
+	cat NVARCHAR(50),
 	subcat NVARCHAR(50),
 	maintenance NVARCHAR(10),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
